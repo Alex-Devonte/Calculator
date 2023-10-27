d970dd542b0229688641a46bfcce3774b814ccdc
Alex <alex95.taylor@gmail.com>
2023-10-26 22:18:26 -0400

    Add number displaying logic

--- calculator.js
+++ calculator.js
@@ -2,8 +2,18 @@ let display = document.querySelector('#display');
 let clearBtn = document.querySelector('.clear');
 let backBtn = document.querySelector('.backspace');
 
+let numbers = document.querySelectorAll('.number');
+
 clearBtn.addEventListener('click', clear);
 backBtn.addEventListener('click', backspace);
+
+numbers.forEach((number)  => { 
+    number.addEventListener('click', function() {
+        console.log(number.textContent);
+        display.textContent += number.textContent;
+    });
+    
+});
 
 function clear() {
     display.textContent = '';
