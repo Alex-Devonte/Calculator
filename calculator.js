86be4251f7905b5f66b24563c6917cf3ad45f5fb
Alex <alex95.taylor@gmail.com>
2023-10-27 20:25:42 -0400

    Add functionality for simple arithmetic
    -Add return values to the functions:(add, subtract, multiply, divide)
    -Add operate function to solve equations based on operator
    -Add individual update functions for the display and equations
    -Modify clear function to reset variables

--- calculator.js
+++ calculator.js
@@ -1,43 +1,111 @@
 let display = document.querySelector('#display');
 let clearBtn = document.querySelector('.clear');
 let backBtn = document.querySelector('.backspace');
+let equalsBtn = document.querySelector('.equals');
 
 let numbers = document.querySelectorAll('.number');
+let operators = document.querySelectorAll('.operator');
+let currOperator = '';
+
+let num1 = '';
+let num2 = '';
 
 clearBtn.addEventListener('click', clear);
 backBtn.addEventListener('click', backspace);
 
 numbers.forEach((number)  => { 
     number.addEventListener('click', function() {
-        console.log(number.textContent);
-        display.textContent += number.textContent;
+        updateDisplay(number.textContent);
+        let displayContent = getDisplayContent();
+        
+        //Set the numbers appropriately based on whether or not the user has clicked an operator
+        if (currOperator == '') {
+            num1 = parseFloat(displayContent);
+        } else {
+            //Slice display at operator to get second number of the equation
+            let index = displayContent.indexOf(currOperator);  
+            num2 = parseFloat(displayContent.slice(index+1));
+        }   
+    });   
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', function() {
+        //Only allow an operator after a number has been entered
+        if (num1 != '') 
+        {
+            //Only allow an operator if there isn't one already to prevent multiple
+            if (currOperator == '') {
+                currOperator = operator.textContent;
+                updateDisplay(currOperator);
+            } 
+        }    
     });
-    
+});
+
+equalsBtn.addEventListener('click', function() {
+    operate(currOperator);
 });
 
 function clear() {
     display.textContent = '';
+    num1 = '';
+    num2 = '';
+    currOperator = '';
 }
 
 function backspace() {
     display.textContent = display.textContent.slice(0, -1);
 }
 
-function add() {
-
+function add(a,b) {
+    return a + b;
 }
 
-function subtract() {
-
+function subtract(a,b) {
+    return a - b;
 }
 
-function multiply() {
-
+function multiply(a,b) {
+    return a * b;
 }
 
-function divide() {
-
+function divide(a,b) {
+    return a / b;
 }
 
+function operate(operator) {
+    switch (operator) {
+        case "+":
+            updateEquation(add(num1, num2));
+            break;
+        case "-":
+            updateEquation(subtract(num1, num2));
+            break;
+        case "*":
+            updateEquation(multiply(num1, num2));
+            break;
+        case "/":
+            updateEquation(divide(num1, num2));
+            break;
+        default:
+            break;
+    }
+}
+
+//Reset display to start with solution to the previous equation
+function updateEquation(val) {
+    clear();
+    updateDisplay(val);
+    num1 = val;
+}
+
+function updateDisplay(content) {
+    display.textContent += content;
+}
+
+function getDisplayContent() {
+    return display.textContent;
+}
 
 
